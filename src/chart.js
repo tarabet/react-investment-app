08ee550ebeaade2ebc50a08c98e168f2be496fdc
Alexander Taranenko <alexander.taranenko@matmatch.com>
2021-04-19 16:09:26 +0200

    Canvas and Chart tests removed

--- src/chart.js
+++ src/chart.js
@@ -125,3 +125,31 @@ Chart.propTypes = {
   supportedRisksCalculations: PropTypes.arrayOf(PropTypes.object),
   riskLevelSelected: PropTypes.number,
 };
+
+// These are commented tests for Chart component. I had to so because it requires 'canvas' dependency to run correctly.
+// Unfortunately there's some mess with this dependency during deployment on AWS. I had to comment it to save time.
+
+// import React from 'react';
+//
+// import { render, screen } from '@testing-library/react';
+// import Chart from './chart';
+//
+// import cones from '../cones.json';
+//
+// describe('Tests menu component', () => {
+//   beforeAll(() => {
+//     render(
+//       <Chart riskLevelSelected={cones[0].riskLevel} supportedRisksCalculations={cones} deposit={1000} />,
+//     );
+//   });
+//
+//   it('Should render chart correctly if all data is provided', () => {
+//     expect(screen.getByTestId('chart')).toBeInTheDocument();
+//   });
+//
+//   it('Should display appropriate message if data is not provided', () => {
+//     render(<Chart />);
+//
+//     expect(screen.getByText('No data is provided')).toBeInTheDocument();
+//   });
+// });
